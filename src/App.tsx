4eeb02795cd6bb64f31dbfe9c64e87b540763127
Kris Nuttycombe <kris@nutty.land>
2023-01-01 22:28:24 -0700

    Begin work on layout capture tools.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef, useState, MouseEventHandler } from 'react';
+import React, { useEffect, useRef, useState, MouseEventHandler, ChangeEventHandler } from 'react';
 import arena from './arena.png';
 import './App.css';
 
-//type Exclusion = {
-//  top_left_x: number;
-//  top_left_y: number;
-//  width: number;
-//  height: number;
-//}
-//
-//type ArenaLayout = {
-//  exclusions: Array<Exclusion>;
-//}
-//
+type Exclusion = {
+  top_left_x: number;
+  top_left_y: number;
+  width: number;
+  height: number;
+}
+
+type ArenaLayout = {
+  exclusions: Array<Exclusion>;
+}
+
 type Location = {
   label: string;
   x: number;
@@ -27,14 +27,14 @@ type ArenaLocations = {
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const Arena: React.FC = () => {
+  const [mode, setMode] = useState<"layout" | "measure">("layout");
   const [locations, setLocations] = useState<ArenaLocations>({ label_idx: 0, locations: [] });
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const handleCanvasClick: MouseEventHandler<HTMLCanvasElement> = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     const rect = event.currentTarget.getBoundingClientRect();
     const loc = { label: labels.charAt(locations.label_idx), x: event.clientX - rect.left, y: event.clientY - rect.top };
-    console.log(`Clicked ${locations.locations.length} times, latest click at x: ${loc.x}, y: ${loc.y}`);
-    setLocations((s) => ({label_idx: s.label_idx + 1, locations: s.locations.concat(loc)}));
+    setLocations((s) => ({ label_idx: s.label_idx + 1, locations: s.locations.concat(loc) }));
   };
 
   const handleMouseDown: MouseEventHandler<HTMLCanvasElement> = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
@@ -45,30 +45,31 @@ const Arena: React.FC = () => {
 
   };
 
-  const handleClearClick: MouseEventHandler<HTMLButtonElement> = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setLocations((s) => ({label_idx: 0, locations: []}));
+  const handleModeSwich: ChangeEventHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setMode("measure");
+    } else {
+      setMode("layout");
+    }
   };
 
-  //const draw = (ctx: CanvasRenderingContext2D) => {
-  //};
+  const handleClearClick: MouseEventHandler<HTMLButtonElement> = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setLocations((s) => ({ label_idx: 0, locations: [] }));
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    console.log("In useEffect");
     if (canvas != null) {
       const ctx = canvas.getContext('2d');
-      canvas.width = 800; 
-      canvas.height = 400; 
-      console.log("Have a canvas");
+      canvas.width = 800;
+      canvas.height = 400;
       if (ctx != null) {
-        console.log(`In draw with ${locations.locations.length} locations.`);
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         locations.locations.forEach((loc) => {
-          console.log(`Writing "${loc.label}" to the canvas at ${loc.x}, ${loc.y}`);
-            ctx.font = "20px Consolas";
-            ctx.fillStyle = "black";
-            ctx.fillText(loc.label, loc.x - 8, loc.y + 5);
-          });
+          ctx.font = "20px Consolas";
+          ctx.fillStyle = "black";
+          ctx.fillText(loc.label, loc.x - 8, loc.y + 5);
+        });
       }
     }
   }, [locations]);
@@ -77,8 +78,15 @@ const Arena: React.FC = () => {
     <div className="arena">
       <div className='arenaWrapper'>
         <div className='arenaInternal'>
-          <img src={arena} className='arenaBackground' alt="Robot Arena"/>
+          <img src={arena} className='arenaBackground' alt="Robot Arena" />
           <canvas ref={canvasRef} className='arenaCanvas' onClick={handleCanvasClick} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}></canvas>
+        </div>
+      </div>
+      <div>
+        <div className='switch_row'>
+          <span className='switch_label'>Layout</span>
+          <input type="checkbox" className='switch' onChange={handleModeSwich} />
+          <span className='switch_label'>Measure</span>
         </div>
       </div>
       <button onClick={handleClearClick}>Clear</button>
